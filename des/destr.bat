19f39dfb18a139e90eaae6529ff227ca35720c4a
JZProYT <159730267+JZProYT@users.noreply.github.com>
2024-03-23 20:08:22 +0100

    Add files via upload

---
+++ des/destr.bat
@@ -0,0 +1,162 @@
+@echo off
+start f11.vbs
+echo Oh, oh...
+echo You have been HACKED!
+pause > nul
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+pause > nul
+
+
---
+++ obj/virus.bat
@@ -0,0 +1,192 @@
+@echo off
+title Fake Death
+echo Your computer will be automatically destroyed in 1 minute...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 59 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 58 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 57 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 56 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 55 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 54 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 53 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 52 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 51 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 50 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 49 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 48 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 47 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 46 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 45 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 44 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 43 seconds..
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 42 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 41 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 40 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 39 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 38 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 37 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 36 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 35 seconds..
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 34 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 33 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 32 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 31 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 30 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 29 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 28 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 27 seconds..
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 26 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 25 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 24 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 23 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 22 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 21 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 20 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 19 seconds..
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 18 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 17 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 16 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 15 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 14 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 13 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 12 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 11 seconds..
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 10 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 9 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 8 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 7 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 6 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 5 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 4 seconds...
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 3 seconds..
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 2 seconds..
+timeout 1 > nul
+cls
+echo Your computer will be automatically destroyed in 1 seconds..
+timeout 1 > nul
+cls
+echo Preparing...
+color 0f
+timeout 1 > nul
+color 07
+timeout 1 > nul
+color 0f
+timeout 1 > nul
+cd..
+cd des
+start destr.bat
